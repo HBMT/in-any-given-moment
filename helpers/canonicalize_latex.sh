9d74ec61ae6578679990ee63431f11367b25778a
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2021-03-18 11:30:22 +0000

    start ebook

--- helpers/canonicalize_latex.sh
+++ helpers/canonicalize_latex.sh
@@ -7,6 +7,8 @@ TEX_SRC="$1"
 cat "$TEX_SRC" | \
 # Converting quote environments. Put the original markup in a role attribute forasciidoc
 perl -0777 -pe "s/\\\\begin\{quote\}(.*?)\\\\end\{quote\}/\\\\begin{quote}% <attr role=quote>\n\\\\itshape \1\\\\end{quote}/gs" | \
+# \begin{quotation} > \begin{quote}
+perl -0777 -pe "s/\\\\begin\{quotation\}(.*?)\\\\end\{quotation\}/\\\\begin{quote}% <attr role=quote>\n\\\\itshape \1\\\\end{quote}/gs" | \
 # \begin{lpchah} > \begin{quote}
 perl -0777 -pe "s/\\\\begin\{lpchah\}(.*?)\\\\end\{lpchah\}/\\\\begin{quote}% <attr role=lpchah>\n\1\\\\end{quote}/gs" | \
 # remove \item from dialogue env
@@ -21,6 +23,8 @@ perl -0777 -pe "s/\\\\begin\{openingVerse\}(.*?)\\\\end\{openingVerse\}/\\\\begi
 perl -0777 -pe "s/\\\\begin\{packeditemize\}(.*?)\\\\end\{packeditemize\}/\\\\begin{itemize}\n\1\\\\end{itemize}/gs" | \
 # glossarydescription
 sed -e 's/\\begin{glossarydescription}/\\begin{description}/; s/\\end{glossarydescription}/\\end{description}/;' | \
+# chapter title with TOC text
+perl -0777 -pe "s/\\\\chapter[[]([^]]+)[]]\{.*\}/\\\\chapter{\1}/gs" | \
 # chapter title and subtitle
 perl -0777 -pe "s/\\\\chapterTitle\{(.*?)\}(.*?)\\\\theChapterTitle/\2\1/gs" | \
 perl -0777 -pe "s/\\\\chapterSubtitle\{(.*?)\}(.*?)\\\\theChapterSubtitle/\2\1/gs" | \
@@ -33,6 +37,7 @@ perl -0777 -pe "s/\\\\chapterNote\{(.*?)\}(.*?\\\\chapter\{.*?\})/\2\n\n\\\\emph
 # toc chapter note
 perl -0777 -pe "s/\\\\tocChapterNote\{.*?\}//gs" | \
 perl -0777 -pe "s/\\\\chapterPhotoTwoPage\{(.*?)\}\{.*?\}\{.*?\}\{.*?\}/\\\\includegraphics{\1}/gs" | \
+perl -0777 -pe "s/\\\\chapterPhotoInlinePortrait[[][^]]+[]]\{.*?\}\{(.*?)\}/\\\\includegraphics{\1}/gs" | \
 perl -0777 -pe "s/\\\\chapterPhotoInlinePortrait\{.*?\}\{(.*?)\}/\\\\includegraphics{\1}/gs" | \
 perl -0777 -pe "s/\\\\chapterPhotoInlineLandscape\{(.*?)\}/\\\\includegraphics{\1}/gs" | \
 perl -0777 -pe "s/\\\\includegraphics[[][^]]+[]]\{.*?\}//gs" | \
@@ -41,6 +46,9 @@ perl -0777 -pe "s/\\\\quoteRef\{(.*?)\}/\n\\\\emph{\1}% <attr attribution=\1>\n/
 perl -0777 -pe "s/\\\\quoteRefInline\{(.*?)\}/\n\\\\emph{\1}% <attr attribution=\1>\n/gs" | \
 perl -0777 -pe "s/\\\\quoteTitleFmt\{(.*?)\}/\\\\emph{\1}/gs" | \
 perl -0777 -pe "s/\\\\thai\{(.*?)\}/\1/gs" | \
+perl -0777 -pe "s/\\\\textls\{(.*?)\}/\1/gs" | \
+perl -0777 -pe "s/\\\\question\{(.*?)\}/\\\\emph{\1}/gs" | \
+perl -0777 -pe "s/\\\\answer\{(.*?)\}/\\\\emph{\1}/gs" | \
 perl -0777 -pe "s/\\\\textup\{(.*?)\}/{\\\\upshape \1}/gs" | \
 perl -0777 -pe "s/\\\\label\{.*?\}//gs" | \
 perl -0777 -pe "s/\\\\enlargethispage\**\{.*?\}//gs" | \
@@ -48,14 +56,17 @@ perl -0777 -pe "s/\\\\setlength\{.*?\}\{.*?\}//gs" | \
 perl -0777 -pe "s/\\\\thispagestyle\{.*?\}\{.*?\}//gs" | \
 perl -0777 -pe "s/\\\\pageref\{.*?\}/FIXME:pageref/gs" | \
 perl -0777 -pe "s/\\\\definecolor\{.*?\}\{.*?\}\{.*?\}//gs" | \
+perl -0777 -pe "s/\\\\cite\{(.*?)\}/\\\\footnote{FIXME:cite \1}/gs" | \
 # Repeating hyphen (in Portuguese)
 sed 's/\(\w\)"-\(\w\)/\1-\2/g' |\
 sed 's/^\\vspace\**[{][^}]\+[}]%*$//g' |\
+sed 's/^\\hspace\**[{][^}]\+[}]%*$//g' |\
 sed 's/\\thinspace\s*/~/g' |\
 sed 's/\\par/\n\n/g' |\
 sed 's/\\mainmatter//g' |\
 sed 's/\\centering//g' |\
 sed 's/\\raggedleft//g' |\
+sed 's/\\raggedright//g' |\
 sed 's/\\clearpage//g' |\
 sed 's/\\parskip//g' |\
 sed 's/\\baselineskip//g' |\
